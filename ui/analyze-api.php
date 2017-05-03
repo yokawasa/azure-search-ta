f443850d8ed0db4fd6bce32a2275b39e7ebd7af5
Yoichi Kawasaki <yoichika@microsoft.com>
2017-05-03 09:36:57 +0900

    Added Azure Search Text Analyze UI

---
+++ ui/analyze-api.php
@@ -0,0 +1,51 @@
+<?php
+
+$azureSearchAccount="<Azure Search Service name>";
+$azureSearchApiKey = "<Azure Search API Admin Key>";
+$azureSearchIndex= "<Azure Search index name>";
+$AZURESEARCH_URL_BASE= 'https://'.$azureSearchAccount.'.search.windows.net/indexes/'.$azureSearchIndex.'/analyze?api-version=2015-02-28-Preview';
+
+$req=$_REQUEST;
+$params = array();
+if( is_array($req) ) {
+    foreach( $req as $name => $value) {
+        $params[$name] = $value;
+    }
+}
+if ( !array_key_exists('text', $params) || !array_key_exists('analyzer', $params) ) {
+    print "Error!";
+    exit;
+}
+$body_arr = array(
+    "text" => $params['text'],
+    "analyzer" => $params['analyzer']
+);
+
+$header = array(
+    "Content-Type: application/json; charset=UTF-8",
+    "Api-Key: ". $azureSearchApiKey,
+    "Accept': application/json",
+    "Accept-Charset: UTF-8"
+);
+$context = array(
+    "http" => array(
+        "method"  => "POST",
+        "header"  => implode("\r\n", $header),
+        "content" => json_encode($body_arr)
+    )
+);
+
+$res_data = file_get_contents($AZURESEARCH_URL_BASE, false, stream_context_create($context));
+
+if ($res_data  === false) {
+    print "Error!";
+}
+else
+{
+    header('Content-Length: '.strlen($res_data));
+    header('Content-Type: application/json; odata.metadata=minimal');
+    header('Access-Control-Allow-Origin: *');
+    print $res_data;
+}
+
+?>
---
+++ ui/azure-search-ta-ui.js
@@ -0,0 +1,35 @@
+var inSearch = false;
+var detailMode= false;
+
+function execSearch()
+{
+	var text = $("#ta_text").val();
+    var analyzer = $("#ta_analyzer").val();
+	var searchAPI1 = "/analyze-api.php";
+	inSearch= true;
+
+    var postdata = {
+       "text": text,
+       "analyzer": analyzer
+    };
+    $.ajax({
+        url: searchAPI1,
+        beforeSend: function (request) {
+            request.setRequestHeader("Accept", "application/json");
+        },
+        data: postdata,
+        type: "POST",
+        success: function (data) {
+
+			$( "#apidata-container" ).html('');
+    		$( "#apidata-container" ).append('<h2>Output Tokens</h2>');
+			for (var item in data.tokens)
+			{
+                $( "#apidata-container" ).append( ' <kbd class=lead>' + data.tokens[item].token + '</kbd> ');
+            }
+			inSearch= false;
+        }
+    });
+
+}
+
